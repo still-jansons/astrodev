41ddfc658a597e3c533974435a6fe92f863e5c70
Arturs <arturs.jansons@va.lv>
2023-01-12 00:05:44 +0200

    Init commit

---
+++ app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="h-screen w-screen glass flex items-center justify-center">
+    <nuxt-picture src="/img/logo.webp" class='max-w-[60%] lg:max-w-[30%]' />
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,18 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    modules: [
+        '@nuxtjs/tailwindcss',
+        '@nuxt/image-edge'
+    ],
+    app: {
+        head: {
+            title: 'AstroDev.',
+            meta: [
+                { name: 'description', content: 'Astronaut Development' }
+            ],
+        }
+    },
+    css: [
+        '@/assets/styles/main.scss'
+    ]
+})
