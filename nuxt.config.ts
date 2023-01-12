03c1e24f246a4277ad783154d65bd6028643a56c
Arturs <arturs.jansons@va.lv>
2023-01-12 02:00:38 +0200

    Change title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
     ],
     app: {
         head: {
-            title: 'AstroDev.',
+            title: 'AstroDev',
             meta: [
                 { name: 'description', content: 'Astronaut Development' }
             ],
